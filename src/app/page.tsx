1f18a9ea864a03af8e4e6dcd8cddd80e850c2245
kyle4401 <hai.phan@navercorp.com>
2023-12-05 15:27:51 +0700

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,25 @@ const MyLineChart = dynamic(() => import('@/app/MyLineChart'), {
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <MyLineChart />
+      {/*   <MyLineChart /> */}
+      <iframe
+        data-tf-redirect-target="_self"
+        src="https://dev-vdc-dashboard.navercorp.com/workspaces/12"
+        height={600}
+        width={600}
+
+        /*         sandbox="allow-top-navigation allow-scripts allow-forms allow-same-origin"
+         */
+      ></iframe>
+      <iframe
+        data-tf-redirect-target="_self"
+        src="https://zing-mp3-clone-eta.vercel.app"
+        height={600}
+        width={600}
+
+        /*         sandbox="allow-top-navigation allow-scripts allow-forms allow-same-origin"
+         */
+      ></iframe>
     </main>
   );
 }

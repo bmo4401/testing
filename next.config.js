c972a9ab33c09916366a0feb004d5b7ff025f132
kyle4401 <hai.phan@navercorp.com>
2023-12-05 13:29:37 +0700

    add

--- next.config.js
+++ next.config.js
@@ -1,17 +1,18 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: 'export',
-  webpack: (config, { isServer }) => {
+  /*   webpack: (config, { isServer }) => {
+    console.log('❄️ ~ file: next.config.js:5 ~ config:', config);
     if (!isServer) {
       // This is to ensure that the code within this block only runs when building the client-side bundle.
       // Exclude the Widget component from the main bundle and create a separate chunk
-      /*       config.optimization.splitChunks.cacheGroups.MyLineChart = {
+      config.optimization.splitChunks.cacheGroups.MyLineChart = {
         test: /[\\/]MyLineChart\.js$/,
         name: 'mychart',
         chunks: 'all',
-      }; */
+      };
     }
 
     return config;
-  },
+  }, */
 };

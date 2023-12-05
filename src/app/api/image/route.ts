25fffde807dde4270b931d3612005e30b23043be
kyle4401 <hai.phan@navercorp.com>
2023-12-05 10:47:08 +0700

    add

--- src/app/api/image/route.ts
+++ src/app/api/image/route.ts
@@ -2,26 +2,29 @@ import { NextResponse } from 'next/server';
 
 export async function GET(req: Request) {
   try {
-    const xmlContent = `<?xml version="1.0" encoding="UTF-8"?>
-    <html xsl:version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-    <body style="font-family:Arial;font-size:12pt;background-color:#EEEEEE">
-    <xsl:for-each select="breakfast_menu/food">
-      <div style="background-color:teal;color:white;padding:4px">
-        <span style="font-weight:bold"><xsl:value-of select="name"/> - </span>
-        <xsl:value-of select="price"/>
-        </div>
-      <div style="margin-left:20px;margin-bottom:1em;font-size:10pt">
-        <p>
-        <xsl:value-of select="description"/>
-        <span style="font-style:italic"> (<xsl:value-of select="calories"/> calories per serving)</span>
-        </p>
-      </div>
-    </xsl:for-each>
+    const xmlContent = `<html>
+    <style>
+    div {
+      background-color: #FFF4A3;
+    }
+    </style>
+    <body>
+    
+    <h1>HTML DIV Example</h1>
+    
+    <div>
+      <h2>London</h2>
+      <p>London is the capital city of England.</p>
+      <p>London has over 13 million inhabitants.</p>
+    </div>
+    
+    <p>The yellow background is added to demonstrate the footprint of the DIV element.</p>
+    
     </body>
     </html>`;
 
     return new NextResponse(xmlContent, {
-      headers: { 'Content-Type': 'text/xml' },
+      headers: { 'Content-Type': 'text/html' },
     });
     /* return NextResponse.json([
       {
